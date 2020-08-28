4b0a82a5be01c67dba22de9a8367d470aae6634f
Arunoda Susiripal <50838+arunoda@users.noreply.github.com>
2020-08-28 15:25:56 +0530

    apply changes

---
+++ pages/dashboard/index.js
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import Theme from '../../components/Theme'
+import ms from 'ms'
+import { getPostList } from '../../lib/data'
+import { getSession } from 'next-auth/client'
+
+export default function Home ({ postList }) {
+  return (
+    <Theme>
+      <div className="dashboard">
+        <div className="heading">
+            <h2>Dashboard</h2>
+            <Link href='/dashboard/post/create'>
+                <a className="create-new">
+                    + Create New
+                </a>
+            </Link>
+        </div>
+        <div className='dashboard-posts'>
+          {postList.map(post => (
+            <div key={post.slug} className='dashboard-post'>
+              <div className='time'>{ms(Date.now() - post.createdAt, { long: true })} ago</div>
+              <h3>{post.title}</h3>
+              <div className="cta">
+                <Link href='/dashboard/post/[slug]' as={`dashboard/post/${post.slug}`}>
+                  <a className="edit">
+                    Edit
+                  </a>
+                </Link>
+                <a className="delete" href="#" onClick={e => handleDelete(e, post)}>
+                  Delete
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Theme>
+  )
+}
+
+export async function getStaticProps ({ req, res }) {
+  const session = await getSession({ req })  
+  const postList = await getPostList({ownerId: session.user.id})
+
+  return {
+    props: {
+      postList
+    }
+  }
+}
