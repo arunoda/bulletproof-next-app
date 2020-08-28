f82a5e433e364a8454e8e12558d9587fe9f46a53
Arunoda Susiripal <50838+arunoda@users.noreply.github.com>
2020-08-28 15:25:54 +0530

    apply changes

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,36 @@
 import Link from 'next/link'
+import { useSession, signIn, signOut } from 'next-auth/client'
 
 export default function Header () {
+  const [session] = useSession()
+
+  console.log('SESSION', session)
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    signIn('github')
+  }
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+    signOut()
+  }
+
   return (
     <div className='header'>
       <Link href='/'>
         <a className='title'>My Blog</a>
       </Link>
+      <div className="user-info">
+        {session? (
+          <>
+            <img src={session.user.image} className="user"/>
+            <a href="#" onClick={handleLogout} className="logout">Logout</a>
+          </>
+        ) : (
+          <a href="#" onClick={handleLogin} className="logout">Login</a>
+        )}
+      </div>
     </div>
   )
 }
