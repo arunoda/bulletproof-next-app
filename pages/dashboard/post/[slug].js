6f3794e2725f231e101b63d8586dddd064dd304a
Arunoda Susiripal <50838+arunoda@users.noreply.github.com>
2020-08-28 15:25:56 +0530

    apply changes

---
+++ pages/dashboard/post/[slug].js
@@ -0,0 +1,67 @@
+import Theme from '../../../components/Theme'
+import { getPost } from '../../../lib/data'
+import { useRouter } from 'next/router'
+import { getSession } from 'next-auth/client'
+import { useState } from 'react'
+import axios from 'axios'
+
+export default function EditPost({ post }) {
+  const [title, setTitle] = useState(post && post.title)
+  const [content, setContent] = useState(post && post.content)
+  const [submitting, setSubmitting] = useState(false)
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+    try {
+      setSubmitting(true)
+      await axios.patch(`/api/post?slug=${encodeURIComponent(post.slug)}`, {
+        title,
+        content
+      })
+      router.push('/dashboard')
+    } catch(err) {
+      return alert(err.message)
+    } finally {
+      setSubmitting(false)
+    }
+  }
+
+  return (
+    <Theme>
+      <div className="dashboard">
+        <form className='edit-post' onSubmit={handleSubmit}>
+          <h2>Edit: {post.title}</h2>
+          <input
+            type="text"
+            placeholder="Add your blog post title"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <br/>
+          <textarea
+            placeholder="Add your blog post content"
+            onChange={e => setContent(e.target.value)}
+            value={content}
+          />
+          <br/>
+          <button disabled={submitting}>
+            {submitting? 'submitting ...' : 'Submit'}
+          </button>
+        </form>
+      </div>
+    </Theme>
+  )
+}
+
+export async function getServerSideProps({ req, res, params }) {
+  const session = await getSession({ req })
+  const post = await getPost(params.slug, { ownerId: session.user.id })
+
+  return {
+    props: {
+      post
+    }
+  }
+}
