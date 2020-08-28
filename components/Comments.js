ae8778b9c8443721d6505202190ceafb20a3d176
Arunoda Susiripal <50838+arunoda@users.noreply.github.com>
2020-08-28 15:25:57 +0530

    apply changes

--- components/Comments.js
+++ components/Comments.js
@@ -11,20 +11,33 @@ async function swrFetcher(path) {
     return res.json()
 }
 
-export default function Comments({slug}) {
+export default function Comments({ slug }) {
+    const pageLimit = 3;
     const getCacheKey = (pageIndex, prevPageData) => {
-        return `/api/comments?slug=${slug}`
+        // This is first page
+        if (pageIndex === 0) {
+            return `/api/comments?slug=${slug}&limit=${pageLimit}`
+        }
+
+        // This is at the end of the list.
+        if (prevPageData.length < pageLimit) {
+            return null
+        }
+
+        // Here we fetch pages using a offset
+        const lastComment = prevPageData[prevPageData.length - 1]
+        return `/api/comments?slug=${slug}&limit=${pageLimit}&offset=${lastComment.createdAt}`
     }
-    
-    const {data, mutate} = useSWRInfinite(
+
+    const { data, mutate, size, setSize } = useSWRInfinite(
         getCacheKey,
         swrFetcher
     );
-    
+
     let comments = null
     if (data) {
         comments = []
-        for(const pageData of data) {
+        for (const pageData of data) {
             comments = [...comments, ...pageData]
         }
     }
@@ -35,8 +48,8 @@ export default function Comments({slug}) {
                 content
             })
             await mutate()
-        } catch(err) {
-            const alertMessage = err.response? err.response.data : err.message
+        } catch (err) {
+            const alertMessage = err.response ? err.response.data : err.message
             alert(alertMessage)
         }
     }
@@ -65,20 +78,30 @@ export default function Comments({slug}) {
                                 <Markdown>{c.error || c.content || ''}</Markdown>
                             </div>
                             <div className="comment-author">
-                                <img src={c.avatar} title={c.name}/>
+                                <img src={c.avatar} title={c.name} />
                                 <div>{c.name} ({ms(Date.now() - c.createdAt)} ago)</div>
                             </div>
                         </div>
                     ))}
+                    <a
+                        href="#"
+                        className="load-more"
+                        onClick={(e) => {
+                            e.preventDefault()
+                            setSize(size + 1)
+                        }}
+                    >
+                        Load More
+                    </a>
                 </div>
             ) : (
-                <div className="comments">
-                    <div className="comments-info">
-                        No comments so far.
+                    <div className="comments">
+                        <div className="comments-info">
+                            No comments so far.
                     </div>
-                </div>
-            )}
-            <AddCommentBox onSubmit={handleAddComment}/>
+                    </div>
+                )}
+            <AddCommentBox onSubmit={handleAddComment} />
         </div>
     )
 }
