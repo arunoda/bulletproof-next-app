224c41bb9f58378f15072c0d9fc796d402d2ab49
Arunoda Susiripal <50838+arunoda@users.noreply.github.com>
2020-08-28 15:25:55 +0530

    apply changes

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,23 +1,31 @@
 import { getComments, addComment } from '../../lib/data'
+import { getSession } from 'next-auth/client'
 
 export default async function comments(req, res) {
-    const {slug} = req.query
+    const { slug } = req.query
 
     if (req.method === 'GET') {
         const comments = await getComments(slug)
         return res.send(comments)
     }
-    
+
     if (req.method === 'POST') {
+        const session = await getSession({ req })
+
+        if (!session) {
+            res.status(401).send('Unauthorized')
+            return;
+        }
+
         const comment = {
-            userId: 'user-id',
-            name: 'The User',
-            avatar: 'https://api.adorable.io/avatars/255/the-user@email.png',
+            userId: session.user.id,
+            name: session.user.profile.name,
+            avatar: session.user.profile.avatar,
             content: req.body.content,
             createdAt: Date.now()
         }
 
-        // Slow the API to demonstrate real-life behavior.
+        // Slow the API to demonstrate real life behavior
         await new Promise((r) => setTimeout(r, 600))
 
         await addComment(slug, comment)
