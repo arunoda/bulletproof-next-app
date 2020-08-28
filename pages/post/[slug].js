22880091ce271421c1fc46c575cbfb6bd5e6eb1d
Arunoda Susiripal <50838+arunoda@users.noreply.github.com>
2020-08-28 15:25:54 +0530

    apply changes

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -26,12 +26,28 @@ export default function Post ({ post }) {
   )
 }
 
-export async function getServerSideProps ({ params }) {
+export async function getStaticPaths() {
+  const postList = await githubCms.getPostList()
+  const paths = postList.map(post => ({
+    params: {
+      slug: post.slug
+    }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+
+export async function getStaticProps ({ params }) {
   const post = await githubCms.getPost(params.slug)
 
   return {
     props: {
       post
-    }
+    },
+    revalidate: 2
   }
 }
